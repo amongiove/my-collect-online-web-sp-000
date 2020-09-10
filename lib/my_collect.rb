d59046c8aa4c37f241759e8295c79d754eeb1308
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-10 20:27:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 #Your method should behave the same way––iterating over the given collection and passing each member to the given block––regardless of the content of the collection or the nature of
 
 array = []
-def my_collect(array) 
+def my_collect(array)
   count = 0
   while count < array.length
     yield array[count]
