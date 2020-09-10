451a6231db2a2baa1f1deb38086f4f6a2e80ebd0
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-10 21:16:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,25 +1,11 @@
 #Your method should behave the same way––iterating over the given collection and passing each member to the given block––regardless of the content of the collection or the nature of
 
 array = []
-# my_collect(array)
-#  return_value = []
-#  count = 0
-#  while count < array.length
-#    yield array[count]
-#    return_value << array[count]
-#    count +=1
-#  end
-#  return_value
-#end
 
 def my_collect(array)
-  counter = 0
-  my_collect = []
-  while counter < array.length
-    array.each do |i|
-    my_collect << i
-    counter += 1
-    end
-    my_collect
+  return_array = []
+  array.each do |i|
+    return array << yield(i)
   end
+  return_array
 end
