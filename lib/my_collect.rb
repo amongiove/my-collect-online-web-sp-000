59c85c0bf2f3667817df5c2f4b285e89d7618b04
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-10 20:21:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,4 @@
 array = []
-def my_collect(array)
-  counter = 0
-  while counter < array.length
-      array.each do |name|
-        name.split(" ").first
-      end
-  end
+my_collect(array) do [i]
+  puts i
 end
